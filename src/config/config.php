6b35036945b117cf246f3c2be361485137263d24
Mathias Munk <mh@morningtrain.dk>
2025-09-03 11:34:05 +0200

    Add controller namespace config

--- src/config/config.php
+++ src/config/config.php
@@ -4,4 +4,5 @@
     ],
     'query_var' => env('ROUTE_QUERY_VAR', 'medley_route'),
     'hash_option' => env('ROUTE_HASH_OPTION', 'medley_route_hash'),
+    'controller_namespace' => env('ROUTE_CONTROLLER_NAMESPACE', '\MedleyApp\Controllers'),
 ];
