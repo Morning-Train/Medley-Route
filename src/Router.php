24c464ceac4807e18192d64740f2b2e92d752b3d
Mathias Munk <mh@morningtrain.dk>
2026-02-23 10:28:45 +0100

    Ignore rewrite rules. Instead test every request

--- src/Router.php
+++ src/Router.php
@@ -7,6 +7,8 @@ use Illuminate\Http\Request;
 use Illuminate\Routing\Route;
 use Symfony\Component\HttpFoundation\Request as SymfonyRequest;
 use Symfony\Component\HttpFoundation\Response as SymfonyResponse;
+use Symfony\Component\HttpKernel\Exception\MethodNotAllowedHttpException;
+use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
 use Symfony\Component\HttpKernel\HttpKernelInterface;
 
 class Router extends \Illuminate\Routing\Router implements HttpKernelInterface
@@ -14,104 +16,24 @@ class Router extends \Illuminate\Routing\Router implements HttpKernelInterface
 
     public function boot()
     {
-        \add_action('init', $this->init(...), 1);
-    }
-
-    public function init()
-    {
-        $this->addRewriteTag();
-        $this->routes->refreshNameLookups();
-        $routesHash = md5(json_encode($this->routes->getRoutes()));
-        if ($routesHash != get_option($this->hashOption())) {
-            // Routes have changed.
-            // Add new rewrite rules
-            $this->addRewriteRules();
-            // Flush rules
-            \flush_rewrite_rules();
-            \update_option($this->hashOption(), $routesHash);
-        }
-
         \add_action('parse_request', $this->matchRequest(...));
-    }
-
-    public function addRewriteTag()
-    {
-        \add_rewrite_tag('%' . $this->routeQueryVar() . '%', '([^/]+)');
-    }
-
-    public function routeQueryVar(): string
-    {
-        return $this->container->make('config')->get('route.query_var');
-    }
-
-    protected function hashOption(): string
-    {
-        return $this->container->make('config')->get('route.hash_option');
-    }
-
-    protected function addRewriteRules()
-    {
-        foreach ($this->getRoutes() as $route) {
-            $this->addRewriteRule($route);
-        }
-    }
-
-    /**
-     * Add necessary rewrite tags and rule
-     *
-     * @param  Route  $route
-     * @return void
-     */
-    protected function addRewriteRule(Route $route)
-    {
-        $path = $this->routeQueryVar() . "=" . \urlencode($route->uri());
-
-        $rewriteParameters = $route->rewriteParameters();
-        $regexedParams = [];
-        // If there are parameters then add rewrite tags for these and prepare the regex for rewrite rule
-        if (! empty($rewriteParameters)) {
-            $i = 0;
-            foreach ($rewriteParameters as $parameter) {
-                // Add the parameter's regex
-                $key = $parameter['name'] . ($parameter['optional'] ? '?' : '');
-                $regexedParams[$key] = $parameter['regex'];
-
-                // Append it to the rewrite paths
-                $path .= "&{$parameter['name']}=\$matches[{$i}]";
-
-                // Add rewrite tag for this parameter so that it is available through query_vars
-                \add_rewrite_tag('%' . $parameter['name'] . '%', $parameter['regex']);
-                $i++;
-            }
-        }
-
-        // The full route regex
-        $reqex = '^' . ltrim(
-                trim(
-                    str_replace(
-                        array_map(fn(string $paramName) => "{" . $paramName . "}", array_keys($regexedParams)),
-                        array_values($regexedParams),
-                        $route->uri())),
-                '/'
-            ) . '$';
-        $query = 'index.php?' . $path;
-        $position = 'top';
-
-        // Add the rule
-        \add_rewrite_rule(
-            $reqex,
-            $query,
-            $position
-        );
     }
 
     protected function matchRequest(\WP $environment)
     {
-        if (empty($environment->query_vars[$this->routeQueryVar()])) {
+        $request = $this->container->make('request');
+        $this->routes->refreshNameLookups();
+
+        try {
+            $route = $this->routes->match($request);
+        } catch (NotFoundHttpException|MethodNotAllowedHttpException $e) {
+            // This is fine. This means that WordPress should handle the request
             return;
         }
 
-        \add_action('template_redirect', $this->onTemplateRedirect(...));
+        if (is_a($route, Route::class)) {
+            \add_action('template_redirect', $this->onTemplateRedirect(...));
+        }
     }
 
     public function onTemplateRedirect(): never
